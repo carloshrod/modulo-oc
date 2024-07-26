27017c4268f45ae4f5a796bd3c0c32b05ee47a58
chrod <chrod.dev@gmail.com>
2024-07-25 19:15:31 -0500

    Update classes names

--- src/components/Toolbar/index.jsx
+++ src/components/Toolbar/index.jsx
@@ -1,32 +1,32 @@
-import { Button } from "antd";
-import Link from "next/link";
-import { BiArrowBack } from "react-icons/bi";
-import { IoAdd, IoDownloadOutline } from "react-icons/io5";
-import styles from "./Toolbar.module.css";
+import { Button } from 'antd';
+import Link from 'next/link';
+import { BiArrowBack } from 'react-icons/bi';
+import { IoAdd, IoDownloadOutline } from 'react-icons/io5';
+import styles from './Toolbar.module.css';
 
 const Toolbar = () => {
-  return (
-    <section className={styles.Toolbar}>
-      <Link href="/" className={styles.GoBack}>
-        <span>
-          <BiArrowBack /> Volver a todas las Obras
-        </span>
-      </Link>
-      <div className={styles.ActionButtons}>
-        <Button
-          type="primary"
-          ghost
-          icon={<IoDownloadOutline size={20} />}
-          iconPosition="end"
-        >
-          Descargar Excel
-        </Button>
-        <Button type="primary" icon={<IoAdd size={24} />}>
-          Generar OC
-        </Button>
-      </div>
-    </section>
-  );
+	return (
+		<section className={styles.toolbar}>
+			<Link href='/' className={styles.goBack}>
+				<span>
+					<BiArrowBack /> Volver a todas las Obras
+				</span>
+			</Link>
+			<div className={styles.actionButtons}>
+				<Button
+					type='primary'
+					ghost
+					icon={<IoDownloadOutline size={20} />}
+					iconPosition='end'
+				>
+					Descargar Excel
+				</Button>
+				<Button type='primary' icon={<IoAdd size={24} />}>
+					Generar OC
+				</Button>
+			</div>
+		</section>
+	);
 };
 
 export default Toolbar;
