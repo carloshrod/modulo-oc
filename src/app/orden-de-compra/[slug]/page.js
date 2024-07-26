198960bf1550ec450dc6d7cede3bcb077e0d40db
chrod <chrod.dev@gmail.com>
2024-07-25 19:12:30 -0500

    Add Drawer component (OC03 - Ver OC)

--- src/app/orden-de-compra/[slug]/page.js
+++ src/app/orden-de-compra/[slug]/page.js
@@ -1,24 +1,59 @@
-"use client";
-import CustomHeader from "@/components/Header";
-import styles from "./PurchaseOrder.module.css";
-import { Layout } from "antd";
-import Datatable from "@/components/Datatable";
-import Toolbar from "@/components/Toolbar";
+'use client';
+import { useState } from 'react';
+import styles from './PurchaseOrder.module.css';
+import { Button, Drawer, Layout } from 'antd';
+import CustomHeader from '@/components/Header';
+import Toolbar from '@/components/Toolbar';
+import Datatable from '@/components/Datatable';
+import { IoDownloadOutline } from 'react-icons/io5';
 
 const { Content } = Layout;
 
 const PurchaseOrder = () => {
-  return (
-    <Layout>
-      <CustomHeader />
-      <Content style={{ padding: "8px 16px" }}>
-        <Toolbar />
-        <div className={styles.TableContainer}>
-          <Datatable />
-        </div>
-      </Content>
-    </Layout>
-  );
+	const [open, setOpen] = useState(false);
+	const [title, setTitle] = useState(false);
+
+	const showDrawer = ocName => {
+		setTitle(ocName);
+		setOpen(true);
+	};
+
+	const onClose = () => {
+		setOpen(false);
+	};
+
+	return (
+		<Layout>
+			<CustomHeader />
+			<Content style={{ padding: '8px 16px' }}>
+				<Toolbar />
+				<div className={styles.tableContainer}>
+					<Datatable showDrawer={showDrawer} />
+				</div>
+			</Content>
+			<Drawer
+				title={title}
+				onClose={onClose}
+				open={open}
+				style={{ backgroundColor: '#E1E1E2' }}
+			>
+				<div className={styles.pdfContainer}>
+					<p style={{ fontSize: 30, fontWeight: 'bold' }}>PDF</p>
+				</div>
+				<div style={{ textAlign: 'end' }}>
+					<Button
+						style={{ backgroundColor: '#fff', fontWeight: 600 }}
+						type='primary'
+						ghost
+						icon={<IoDownloadOutline size={20} />}
+						iconPosition='end'
+					>
+						Descargar PDF
+					</Button>
+				</div>
+			</Drawer>
+		</Layout>
+	);
 };
 
 export default PurchaseOrder;
--- src/components/Actions/index.jsx
+++ src/components/Actions/index.jsx
@@ -1,44 +1,45 @@
-const { Space, Tooltip, Button } = require("antd");
-import { IoDocumentTextOutline } from "react-icons/io5";
-import { TbPencilMinus } from "react-icons/tb";
-import { AiOutlineDelete } from "react-icons/ai";
+import { Button, Space, Tooltip } from 'antd';
+import { IoDocumentTextOutline } from 'react-icons/io5';
+import { TbPencilMinus } from 'react-icons/tb';
+import { AiOutlineDelete } from 'react-icons/ai';
 
-export const Actions = ({ record }) => {
-  const notDisabled =
-    record.oc_status === "Rechazada" || record.oc_status === "Borrador";
+export const Actions = ({ record, showDrawer }) => {
+	const notDisabled =
+		record.oc_status === 'Rechazada' || record.oc_status === 'Borrador';
 
-  return (
-    <Space size="small">
-      <Tooltip title="Ver OC">
-        <Button
-          type="text"
-          icon={<IoDocumentTextOutline size={20} color="#0D6EFD" />}
-        />
-      </Tooltip>
-      <Tooltip title={notDisabled ? "Editar OC" : ""}>
-        <Button
-          type="text"
-          icon={
-            <TbPencilMinus
-              size={20}
-              color={notDisabled ? "#0D6EFD" : "#A0AEC0"}
-            />
-          }
-          disabled={!notDisabled}
-        />
-      </Tooltip>
-      <Tooltip title={notDisabled ? "Eliminar OC" : ""}>
-        <Button
-          type="text"
-          icon={
-            <AiOutlineDelete
-              size={20}
-              color={notDisabled ? "#E53535" : "#FCBABA"}
-            />
-          }
-          disabled={!notDisabled}
-        />
-      </Tooltip>
-    </Space>
-  );
+	return (
+		<Space size='small'>
+			<Tooltip title='Ver OC'>
+				<Button
+					type='text'
+					icon={<IoDocumentTextOutline size={20} color='#0D6EFD' />}
+					onClick={() => showDrawer(record.oc_name)}
+				/>
+			</Tooltip>
+			<Tooltip title={notDisabled ? 'Editar OC' : ''}>
+				<Button
+					type='text'
+					icon={
+						<TbPencilMinus
+							size={20}
+							color={notDisabled ? '#0D6EFD' : '#A0AEC0'}
+						/>
+					}
+					disabled={!notDisabled}
+				/>
+			</Tooltip>
+			<Tooltip title={notDisabled ? 'Eliminar OC' : ''}>
+				<Button
+					type='text'
+					icon={
+						<AiOutlineDelete
+							size={20}
+							color={notDisabled ? '#E53535' : '#FCBABA'}
+						/>
+					}
+					disabled={!notDisabled}
+				/>
+			</Tooltip>
+		</Space>
+	);
 };
