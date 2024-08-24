9a66832416cfea31560116c1dd86e6185315b45f
chrod <chrod.dev@gmail.com>
2024-08-24 16:21:05 -0500

    Handle errors in OeuvresPage

--- src/app/orden-de-compra/page.js
+++ src/app/orden-de-compra/page.js
@@ -3,27 +3,36 @@ import axios from 'axios';
 import styles from './page.module.css';
 
 const OeuvresPage = async () => {
-	const res = await axios.get('http://localhost:4000/api/v1/oeuvres');
-	const oeuvres = res.data;
+	const getAllOeuvres = async () => {
+		try {
+			const res = await axios.get('http://localhost:4000/api/v1/oeuvres');
+			return res.data;
+		} catch (error) {
+			console.error(error);
+		}
+	};
+	const oeuvres = await getAllOeuvres();
 
 	return (
 		<section className={styles.home}>
 			<h3>Selecciona una obra:</h3>
 			<div className={styles.linksContainer}>
-				{oeuvres.length > 0
-					? oeuvres.map(item => {
-							const slug = item.oeuvre_name.toLowerCase().split(' ').join('-');
-							return (
-								<Link
-									className={styles.oeuvreLinks}
-									key={item.id}
-									href={`/orden-de-compra/${slug}`}
-								>
-									<span>{item.oeuvre_name}</span>
-								</Link>
-							);
-						})
-					: null}
+				{oeuvres?.length > 0 ? (
+					oeuvres.map(item => {
+						const slug = item.oeuvre_name.toLowerCase().split(' ').join('-');
+						return (
+							<Link
+								className={styles.oeuvreLinks}
+								key={item.id}
+								href={`/orden-de-compra/${slug}`}
+							>
+								<span>{item.oeuvre_name}</span>
+							</Link>
+						);
+					})
+				) : (
+					<h3>¡No hay obras para mostrar!</h3>
+				)}
 			</div>
 		</section>
 	);
