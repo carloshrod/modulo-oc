cc5a78c4642478266a5e21fa11f455ebad177420
chrod <chrod.dev@gmail.com>
2024-08-24 16:13:59 -0500

    Show all oeuvres in OeuvresPage

--- src/app/orden-de-compra/page.js
+++ src/app/orden-de-compra/page.js
@@ -1,13 +1,30 @@
-import styles from './page.module.css';
 import Link from 'next/link';
+import axios from 'axios';
+import styles from './page.module.css';
 
-const OeuvresPage = () => {
-	const oeuvre = 'xxx-calle-santa-julia';
+const OeuvresPage = async () => {
+	const res = await axios.get('http://localhost:4000/api/v1/oeuvres');
+	const oeuvres = res.data;
 
 	return (
 		<section className={styles.home}>
 			<h3>Selecciona una obra:</h3>
-			<Link href={`/orden-de-compra/${oeuvre}`}>XXX Calle Santa Julia</Link>
+			<div className={styles.linksContainer}>
+				{oeuvres.length > 0
+					? oeuvres.map(item => {
+							const slug = item.oeuvre_name.toLowerCase().split(' ').join('-');
+							return (
+								<Link
+									className={styles.oeuvreLinks}
+									key={item.id}
+									href={`/orden-de-compra/${slug}`}
+								>
+									<span>{item.oeuvre_name}</span>
+								</Link>
+							);
+						})
+					: null}
+			</div>
 		</section>
 	);
 };
