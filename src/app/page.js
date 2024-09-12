db09318f88dbd6ba7e10b7510d1829222176541c
chrod <chrod.dev@gmail.com>
2024-09-12 13:01:57 -0500

    Simulate login

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,77 @@
 'use client';
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import { Button } from 'antd';
+import useGlobalContext from '@/hooks/useGlobalContext';
+import {
+	approver1,
+	approver2,
+	approver3,
+	approver4,
+	noApprover,
+} from '@/context/global/users';
 
 export default function Home() {
 	const router = useRouter();
+	const { loggedUser, setLoggedUser } = useGlobalContext();
 
 	useEffect(() => {
-		router.push('/orden-de-compra');
-	}, []);
+		if (loggedUser?.id) {
+			router.push('/orden-de-compra');
+		}
+	}, [loggedUser?.id]);
 
 	return (
-		<main style={{ padding: 16 }}>
+		<main
+			style={{ display: 'flex', flexDirection: 'column', gap: 16, padding: 24 }}
+		>
 			<h3>Home</h3>
+			<h4>Iniciar sesión como:</h4>
+			<Button
+				type='primary'
+				size='large'
+				ghost
+				style={{ width: 130 }}
+				onClick={() => setLoggedUser(noApprover)}
+			>
+				No Aprobador
+			</Button>
+			<Button
+				type='primary'
+				size='large'
+				ghost
+				style={{ width: 130 }}
+				onClick={() => setLoggedUser(approver1)}
+			>
+				Aprobador 1
+			</Button>
+			<Button
+				type='primary'
+				size='large'
+				ghost
+				style={{ width: 130 }}
+				onClick={() => setLoggedUser(approver2)}
+			>
+				Aprobador 2
+			</Button>
+			<Button
+				type='primary'
+				size='large'
+				ghost
+				style={{ width: 130 }}
+				onClick={() => setLoggedUser(approver3)}
+			>
+				Aprobador 3
+			</Button>
+			<Button
+				type='primary'
+				size='large'
+				ghost
+				style={{ width: 130 }}
+				onClick={() => setLoggedUser(approver4)}
+			>
+				Aprobador 4
+			</Button>
 		</main>
 	);
 }
---
+++ src/context/global/users.js
@@ -0,0 +1,29 @@
+export const noApprover = {
+	id: 6,
+	email: 'rquezada@amatistaconsultores.cl',
+	full_name: 'Rodrigo Quezada',
+};
+
+export const approver1 = {
+	id: 8,
+	email: 'chernandezr912@gmail.com',
+	full_name: 'Carlos Hernández',
+};
+
+export const approver2 = {
+	id: 3,
+	email: 'chrod.dev@gmail.com',
+	full_name: 'Pedro Pérez',
+};
+
+export const approver3 = {
+	id: 4,
+	email: 'chernandezr912@gmail.com',
+	full_name: 'Marco Oviedo',
+};
+
+export const approver4 = {
+	id: 1,
+	email: 'chrod.dev@gmail.com',
+	full_name: 'Paula Cortés',
+};
