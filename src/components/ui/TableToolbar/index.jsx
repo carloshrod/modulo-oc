6c2fcaeb2e9a316c68e5e86f4e7128f6237eb252
chrod <chrod.dev@gmail.com>
2024-09-22 17:14:10 -0500

    Disable generate oc when logged user is the last approver

--- src/components/ui/TableToolbar/index.jsx
+++ src/components/ui/TableToolbar/index.jsx
@@ -1,13 +1,18 @@
-import { Button } from 'antd';
+import { Button, Tooltip } from 'antd';
 import { IoAdd, IoDownloadOutline } from 'react-icons/io5';
 import { downloadExcel } from '@/utils/utils';
 import styles from './Toolbar.module.css';
+import usePurchaseOrderContext from '@/hooks/usePurchaseOrderContext';
 
 const TableToolbar = ({ table, excelData, onClick }) => {
+	const { loggedUser } = usePurchaseOrderContext();
 	const BTN_LABELS = {
 		oc: 'Generar OC',
 		receipts: 'Recibir OC',
 	};
+
+	const disableGenerateOc =
+		table === 'oc' && loggedUser?.approver_role === 'approver4';
 
 	return (
 		<section className={styles.toolbar}>
@@ -21,9 +26,22 @@ const TableToolbar = ({ table, excelData, onClick }) => {
 				>
 					Descargar Excel
 				</Button>
-				<Button type='primary' icon={<IoAdd size={30} />} onClick={onClick}>
-					{BTN_LABELS[table]}
-				</Button>
+				<Tooltip
+					title={
+						disableGenerateOc
+							? 'No puedes generar OCs porque eres el último aprobador'
+							: ''
+					}
+				>
+					<Button
+						type='primary'
+						icon={<IoAdd size={30} />}
+						onClick={onClick}
+						disabled={disableGenerateOc}
+					>
+						{BTN_LABELS[table]}
+					</Button>
+				</Tooltip>
 			</div>
 		</section>
 	);
